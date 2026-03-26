eeafdb0a3740ade91e5a322a90c984b83b38e469
eltrueno <truenodeveloper@gmail.com>
2026-03-26 21:45:38 +0100

    Changed totw calculation cron job time

--- apps/worker/src/jobs/totw.job.ts
+++ apps/worker/src/jobs/totw.job.ts
@@ -9,26 +9,20 @@ dotenv.config()
 const FORCE_RECALCULATE = process.env.FORCE_RECALCULATE === "true"
 const TIMEZONE = process.env.TZ || "Europe/Madrid"
 
-const getISOWeekKey = (date = new Date()) => {
-    const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
-
-    const dayNum = d.getUTCDay() || 7
-    d.setUTCDate(d.getUTCDate() + 4 - dayNum)
-
-    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1))
-    const week = Math.ceil(((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7)
-
-    const year = d.getUTCFullYear()
-
-    return `${year}-${String(week).padStart(2, "0")}`
-}
-
 const getPreviousWeekKey = () => {
     const now = new Date()
     const tzNow = toZonedTime(now, TIMEZONE)
     const lastWeek = subWeeks(tzNow, 1)
     const week = getISOWeek(lastWeek)
     const year = getISOWeekYear(lastWeek)
+    return `${year}-${String(week).padStart(2, "0")}`
+}
+
+const getCurrentWeekKey = () => {
+    const now = new Date()
+    const tzNow = toZonedTime(now, TIMEZONE)
+    const week = getISOWeek(tzNow)
+    const year = getISOWeekYear(tzNow)
     return `${year}-${String(week).padStart(2, "0")}`
 }
 
@@ -49,9 +43,10 @@ export const scheduleTOTWJob = () => {
         console.info("[TOTW] Force recalculate enabled, running now...")
         checkMissedTOTW()
     } else console.log("[TOTW] Force recalculate disabled, skipping...")
-    cron.schedule("0 0 * * 1", async () => {
+    //Domingos a las 22:00
+    cron.schedule("0 22 * * 0", async () => {
         console.info("[TOTW] Calculating team of the week...")
-        const week = getPreviousWeekKey()
+        const week = getCurrentWeekKey()
         await calculateAndSaveTOTW(week)
         console.info("[TOTW] Done")
     }, {
